fa636b567b4116bc216101fc2259533535b7903c
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-02-09 07:53:12 -0500

    level logs in .properties

--- src/main/java/com/jhon/springboot/jhon/JhonApplication.java
+++ src/main/java/com/jhon/springboot/jhon/JhonApplication.java
@@ -5,6 +5,8 @@ import com.jhon.springboot.jhon.bean.MyBeanWithDependency;
 import com.jhon.springboot.jhon.bean.MyBeanWithProperties;
 import com.jhon.springboot.jhon.component.ComponentDependecy;
 import com.jhon.springboot.jhon.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JhonApplication implements CommandLineRunner {
-
+	private final Log LOGGER = LogFactory.getLog(JhonApplication.class);
 	private ComponentDependecy componentDependecy;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
@@ -40,5 +42,6 @@ public class JhonApplication implements CommandLineRunner {
 		myBeanWithDependency.printWithDepency();
 		System.out.println(myBeanWithProperties.function());
 		System.out.println(userPojo.getEmail()+"-"+userPojo.getPassword());
+		LOGGER.error("error--->");
 	}
 }
--- src/main/java/com/jhon/springboot/jhon/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/jhon/springboot/jhon/bean/MyBeanWithDependencyImplement.java
@@ -1,15 +1,19 @@
 package com.jhon.springboot.jhon.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
 
    private MyOperation myOperation;
-
-    public MyBeanWithDependencyImplement(MyOperation myOperation) {
+   Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
+   public MyBeanWithDependencyImplement(MyOperation myOperation) {
         this.myOperation = myOperation;
     }
 
     @Override
     public void printWithDepency() {
+       LOGGER.info("Init printWithDepency");
         System.out.println(myOperation.sum(15));
         System.out.println("Hello from bean with dependency");
     }

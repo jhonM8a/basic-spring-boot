716fe3cdd9cfa5aba4607db9fcdc9b638cfcfbe4
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-02-09 07:11:19 -0500

    Chnges to use value of .properties

--- src/main/java/com/jhon/springboot/jhon/JhonApplication.java
+++ src/main/java/com/jhon/springboot/jhon/JhonApplication.java
@@ -2,6 +2,7 @@ package com.jhon.springboot.jhon;
 
 import com.jhon.springboot.jhon.bean.MyBean;
 import com.jhon.springboot.jhon.bean.MyBeanWithDependency;
+import com.jhon.springboot.jhon.bean.MyBeanWithProperties;
 import com.jhon.springboot.jhon.component.ComponentDependecy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -14,11 +15,15 @@ public class JhonApplication implements CommandLineRunner {
 	private ComponentDependecy componentDependecy;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
 
-	public JhonApplication(@Qualifier("componentTwoImplement") ComponentDependecy componentDependecy, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
+	public JhonApplication(@Qualifier("componentTwoImplement") ComponentDependecy componentDependecy,
+						   MyBean myBean, MyBeanWithDependency myBeanWithDependency,
+						   MyBeanWithProperties myBeanWithProperties){
 		this.componentDependecy = componentDependecy;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JhonApplication.class, args);
@@ -29,5 +34,6 @@ public class JhonApplication implements CommandLineRunner {
 		componentDependecy.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDepency();
+		System.out.println(myBeanWithProperties.function());
 	}
 }

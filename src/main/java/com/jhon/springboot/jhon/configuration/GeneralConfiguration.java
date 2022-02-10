d6babb21d9a144da91d80d6612f6c4c9ac5ea183
jhonM8a <jhon.ochoa@unillanos.edu.co>
2022-02-10 07:19:22 -0500

    conf datasource con properties and class

--- src/main/java/com/jhon/springboot/jhon/configuration/GeneralConfiguration.java
+++ src/main/java/com/jhon/springboot/jhon/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.jhon.springboot.jhon.bean.MyBeanWithPropertiesImplement;
 import com.jhon.springboot.jhon.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)//Habilita la clase pojo para inyectar
@@ -23,5 +26,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name,lastName);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+
+        return dataSourceBuilder.build();
     }
 }
